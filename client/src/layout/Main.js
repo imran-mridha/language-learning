eba70e2aa14a7fab1e61e4522d10fc15317c7955
imran-mridha <mdimranmridha.pro@gmail.com>
2022-11-17 11:10:54 +0600

    Final Update

--- client/src/layout/Main.js
+++ client/src/layout/Main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet} from 'react-router-dom';
 import Footer from '../shared/Footer/Footer';
 import Header from '../shared/Header/Header';
+import { ScrollRestoration } from "react-router-dom";
 
 const Main = () => {
   return (
     <div>
       <Header />
+      <ScrollRestoration />
       <Outlet />
       <Footer />
     </div>
